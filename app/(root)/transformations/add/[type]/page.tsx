1fa667eb581477412098405acca8f8d92fcf49e4
umair <sumairn.1997@gmail.com>
2024-05-30 15:53:37 +0500

    Adds transformation type page and image form

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,19 @@
 import Header from "@/components/shared/Header";
 import TransformationForm from "@/components/shared/TransformationForm";
 import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+const AddTransformationTypePage = async ({
+    params: { type },
+}: SearchParamProps) => {
+    const { userId } = auth();
     const transformations = transformationTypes[type];
+
+    if (!userId) redirect("/sign-in");
+
+    const user = await getUserById(userId);
 
     return (
         <>
@@ -12,7 +22,14 @@ const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
                 subtitle={transformations.subTitle}
             />
 
-            <TransformationForm />
+            <section className="mt-10">
+                <TransformationForm
+                    action="Add"
+                    userId={user._id}
+                    type={transformations.type as TransformationTypeKey}
+                    creditBalance={user.creditBalance}
+                />
+            </section>
         </>
     );
 };
