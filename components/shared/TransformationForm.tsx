9cf43e110750973b1fb547ab1486f4f72cd2cfae
umair <sumairn.1997@gmail.com>
2024-06-02 01:17:05 +0500

    Image upload using cloudinary upload widget

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -31,6 +31,7 @@ import { Input } from "@/components/ui/input";
 import { CustomField } from "./CustomField";
 import { useState, useTransition } from "react";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
+import MediaUploader from "./MediaUploader";
 
 export const formSchema = z.object({
     title: z.string(),
@@ -229,6 +230,23 @@ const TransformationForm = ({
                         )}
                     </div>
                 )}
+
+                <div className="media-uploader-field">
+                    <CustomField
+                        control={form.control}
+                        name="publicId"
+                        className="flex size-full flex-col"
+                        render={({ field }) => (
+                            <MediaUploader
+                                onValueChange={field.onChange}
+                                setImage={setImage}
+                                publicId={field.value}
+                                image={image}
+                                type={type}
+                            />
+                        )}
+                    />
+                </div>
 
                 <div className="flex flex-col gap-4">
                     <Button
