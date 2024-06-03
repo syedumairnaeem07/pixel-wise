16b3a0cf8b794af8882ba6b1e357cd2389562478
umair <sumairn.1997@gmail.com>
2024-06-04 01:12:17 +0500

    Adds image crud server actions

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -32,6 +32,8 @@ import { CustomField } from "./CustomField";
 import { useState, useTransition } from "react";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import MediaUploader from "./MediaUploader";
+import TransformedImage from "./TransformedImage";
+import { updateCredits } from "@/lib/actions/user.actions";
 
 export const formSchema = z.object({
     title: z.string(),
@@ -125,7 +127,7 @@ const TransformationForm = ({
         setNewTransformation(null);
 
         startTransition(async () => {
-            // await updateCredits(userId, creditFee);
+            await updateCredits(userId, -1);
         });
     };
 
@@ -245,6 +247,15 @@ const TransformationForm = ({
                                 type={type}
                             />
                         )}
+                    />
+
+                    <TransformedImage
+                        image={image}
+                        type={type}
+                        title={form.getValues().title}
+                        isTransforming={isTransforming}
+                        setIsTransforming={setIsTransforming}
+                        transformationConfig={transformationConfig}
                     />
                 </div>
 
